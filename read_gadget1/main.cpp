eaf411d5e0a556d071f27159cecaea079b4e67b3
Mordi <Mordi@Mordi.com>
2015-06-26 15:47:29 +0100

    Updated read gadget methods

--- read_gadget1/main.cpp
+++ read_gadget1/main.cpp
@@ -13,51 +13,121 @@ template <typename T> std::string to_string(T value){
 }
 
 //////////////////////////////////////
-// Read contents of a GADGET1 file
+// Store the positions of the particles
 //////////////////////////////////////
 
-void read_single_gadget_file(int filenum){
-  std::string filename = "/Users/winther/ahf_test_data/gadget." + to_string(filenum);
+void store_position(float *f, float boxsize, int npart_now, int npart_read){
+  double x,y,z;
+
+  // Verbose
+  std::cout << "Read positions (First particle x/B = ";
+  std::cout << f[0]/boxsize << " y/B = " << f[1]/boxsize << " z/B = " << f[2]/boxsize <<  std::endl;
+
+  for(int i=0;i<npart_now;i++){
+    // The current particle is number 'npart_read + i'
+
+    // Position (x,y,z)
+    x = f[3*i];
+    y = f[3*i+1];
+    z = f[3*i+2];
+
+    // Store the data
+    // ...
+  }
+}
+
+//////////////////////////////////////
+// Store the velocities of the particles
+//////////////////////////////////////
+
+void store_velocity(float *f, int npart_now, int npart_read){
+  double vx,vy,vz;
+
+  // Verbose
+  std::cout << "Read velocities (First particle vx [km/s] = ";
+  std::cout << f[0] << " vy [km/s] = " << f[1] << " vz [km/s] = " << f[2] <<  std::endl;
+
+  for(int i=0;i<npart_now;i++){
+    // The current particle is number 'npart_read + i'
+
+    // Velocity (vx,vy,vz)
+    vx = f[3*i];
+    vy = f[3*i+1];
+    vz = f[3*i+2];
+
+    // Store the data
+    // ...
+  }
+}
+
+//////////////////////////////////////
+// Read contents of a single GADGET1 file
+//////////////////////////////////////
+
+void read_single_gadget_file(std::string fileprefix, int filenum, int *numfiles, int *npart_read){
+  std::string filename = fileprefix + to_string(filenum);
   FILE *fp;
   char *buffer;
-  int npart_now, NDIM = 3;
+  int npart_now;
   float *f;
+  bool verbose = false;
 
   // Open file
   fp = fopen(filename.c_str(),"r");
 
-  // Read header
-  read_gadget_header(fp, true);
+  // Read header and print it for the first file only
+  if(filenum==0)
+    read_gadget_header(fp, true);
+  else
+    read_gadget_header(fp, false);
+
+  // Get number of files
+  *numfiles = header.num_files;
 
   // Allocate memory
   npart_now = header.npart[1];;
-  buffer = new char[sizeof(float) * npart_now * NDIM];
+  buffer = new char[sizeof(float) * npart_now * 3];
   f = (float *) buffer;
 
-  std::cout << "Npart = " << npart_now << std::endl;
+  // Verbose
+  std::cout << std::endl;
+  std::cout << "Reading file " << filenum+1 << " / " << *numfiles << std::endl;
+  std::cout << "Npart_file = " << npart_now << " Numfiles = " << *numfiles << " Npart_read = " << *npart_read << std::endl;
 
   // Read positions
-  std::cout << "Read positions: " << std::endl;
-  read_gadget_float_vector(fp, buffer, NDIM, npart_now, true);
-  for(int i=0;i<npart_now;i++)
-    for(int k=0;k<NDIM;k++){
-      // Do something with the data
-      // Position x[k] of particle 'i' is f[3*i+k]
-    }
+  read_gadget_float_vector(fp, buffer, 3, npart_now, verbose);
+  store_position(f, header.BoxSize, npart_now, *npart_read);
 
   // Read velocity
-  std::cout << "Read velocity: " << std::endl;
-  read_gadget_float_vector(fp, buffer, NDIM, npart_now, true);
-  for(int i=0;i<npart_now;i++)
-    for(int k=0;k<NDIM;k++){
-      // Do something with the data
-      // Velocity v[k] of particle 'i' is f[3*i+k]
-    }
+  read_gadget_float_vector(fp, buffer, 3, npart_now, verbose);
+  store_velocity(f, npart_now, *npart_read);
+
+  // Update how many particles in total we have read
+  *npart_read += npart_now;
 
   fclose(fp);
   delete[] buffer;
 }
 
-int main(int argc, char **argv){
-  read_single_gadget_file(0);
+//////////////////////////////////////
+// Read all GADGET1 files
+//////////////////////////////////////
+
+void read_all_gadget_files(std::string fileprefix){
+  int nfiles, npart_read = 0;
+
+  // Read first file to get the number of output files
+  read_single_gadget_file(fileprefix, 0, &nfiles, &npart_read);
+
+  // Loop over all the other files
+  for(int i=1;i<nfiles;i++)
+    read_single_gadget_file(fileprefix, i, &nfiles, &npart_read);
+
+  // Verbose
+  std::cout << std::endl;
+  std::cout << "Done reading. We have read a total of " << npart_read << " particles" << std::endl;
+}
+
+int main(int argc, char **argv){ 
+  read_all_gadget_files("/Users/winther/mg_comp/lcdm_z0.0/gadget.");
 }
